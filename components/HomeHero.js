033ab81cb7550fdc6c3fe22aaa2a062c5a4786f6
manishamal <malmanisha298@gmail.com>
2021-05-04 20:07:56 +0530

    fixed

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -27,7 +27,7 @@ const HomeHero = () => {
 
                 <span className="navbar-item">
                   <a className="button has-text-white is-link is-outlined">
-                    <span className="">
+                    <span>
                       <i class="fab fa-github"></i> View saurce
                     </span>
                   </a>
@@ -40,8 +40,9 @@ const HomeHero = () => {
 
       <div className="hero-body">
         <div className="container has-text-centered">
-          <p className="title">Title</p>
-          <p className="subtitle">Subtitle</p>
+        <h1 className="is-size-3 has-text-weight-semibold">The new standard in insert industry here</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
         </div>
       </div>
     </section>
