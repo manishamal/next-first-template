765b8881d589dea830435d3a9f00c38174ab7604
manishamal <malmanisha298@gmail.com>
2021-05-04 17:11:29 +0530

    fixed

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,8 +1,8 @@
 const HomeHero = () => {
   return (
-    <section className="hero is-info is-large">
+    <section className="hero is-info is-large is-border">
       <div className="hero-head">
-        <nav className="navbar">
+        <nav className="navbar ">
           <div className="container">
             <div className="navbar-brand">
               <a className="navbar-item">
@@ -19,14 +19,17 @@ const HomeHero = () => {
             </div>
             <div id="navbarMenuHeroB" className="navbar-menu">
               <div className="navbar-end">
-                <a className="navbar-item is-active">Home</a>
-                <a className="navbar-item  has-text-danger">Examples</a>
+                <a className="navbar-item">Home</a>
+                <a className="navbar-item">Examples</a>
                 <a className="navbar-item">Features</a>
                 <a className="navbar-item">Team</a>
                 <a className="navbar-item">Help</a>
+
                 <span className="navbar-item">
                   <a className="button has-text-white is-link is-outlined">
-                    <span className="">View saurce</span>
+                    <span className="">
+                      <i class="fab fa-github"></i> View saurce
+                    </span>
                   </a>
                 </span>
               </div>
@@ -40,33 +43,6 @@ const HomeHero = () => {
           <p className="title">Title</p>
           <p className="subtitle">Subtitle</p>
         </div>
-      </div>
-
-      <div className="hero-foot">
-        <nav className="tabs is-boxed is-fullwidth">
-          <div className="container">
-            <ul>
-              <li className="is-active">
-                <a>Overview</a>
-              </li>
-              <li>
-                <a>Modifiers</a>
-              </li>
-              <li>
-                <a>Grid</a>
-              </li>
-              <li>
-                <a>Elements</a>
-              </li>
-              <li>
-                <a>Components</a>
-              </li>
-              <li>
-                <a>Layout</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
       </div>
     </section>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css';
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import 'bulma/css/bulma.css';
 
 function MyApp({ Component, pageProps }) {
