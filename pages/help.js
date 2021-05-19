090c63108ebe6b100a5e47390405f7b0c5ad1879
manishamal <malmanisha298@gmail.com>
2021-05-19 13:22:58 +0530

    fixed

--- pages/help.js
+++ pages/help.js
@@ -1,10 +1,27 @@
 const help = () => {
   return (
-    <section className="section">
+    <section className="section has-background-color-blue hero-is-medium hero is-fullheight has-text-white">
       <div className="container">
-        <figure class="image is-250x250">
-          <img src="https://mlpxhq8ztvyc.i.optimole.com/QgmSm9c.BePn~446a4/w:475/h:518/q:100/https://thrivethemes.com/wp-content/uploads/2020/10/17-Simple-Sticky-Ribbon-Announcement-Bar-Anchor-Tablet.jpg" />
-        </figure>
+        <div class="columns ">
+          <div class="column ">
+            <h1 class="title has-text-white">How i can help you:-</h1>
+
+            <p className="is-size-4">
+              This is form design type called "ribbon".It is display on the top
+              of the page and it's usually a long horizontal bar. the content of
+              the page will be scrolled down with the same amount as the
+              ribbon's height.the ribbon will always stay on top even when the
+              user scrolls the page.
+            </p>
+            <p className="is-size-4">
+              This is form design type called "ribbon".It is display on the top
+              of the page and it's usually a long horizontal bar. the content of
+              the page will be scrolled down with the same amount as the
+              ribbon's height.the ribbon will always stay on top even when the
+              user scrolls the page.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
